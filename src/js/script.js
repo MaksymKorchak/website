67606643269b4bc76752ea2dc643740055a4f335
Makym Korchak <Mkorchak1992@gmail.com>
2020-09-16 11:53:27 +0300

    some info swiper

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,23 @@ const menu = document.getElementsByClassName('header__section-menu')[0];
 button.addEventListener('click', (ev) => {
   button.classList.toggle('active');
   menu.classList.toggle('mobile');
-})
+});
 
+let swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  speed: 10000,
+  autoplay: {
+    delay: 5,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
