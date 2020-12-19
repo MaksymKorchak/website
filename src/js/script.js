eb5c989ca3e55638818f224dcfd8fd8fc0ac7a8b
Maksym Korchak <mkorchak1992@gmail.com>
2020-12-19 16:18:14 +0200

    some changes in swiper

--- src/js/script.js
+++ src/js/script.js
@@ -7,13 +7,40 @@ button.addEventListener('click', (ev) => {
   menu.classList.toggle('mobile');
 });
 
-let swiper = new Swiper('.swiper-container', {
-  effect: 'fade',
+// let swiper = new Swiper('.swiper-container', {
+//   effect: 'fade',
+//   loop: true,
+//   runCallbacksOnInit:true,
+//   speed: 5000,
+//   autoplay: {
+//     delay: 5000,
+//     disableOnInteraction: false,
+//   }
+// });
+
+let galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+let galleryTop = new Swiper('.gallery-top', {
   loop: true,
-  runCallbacksOnInit:true,
-  speed: 5000,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
+  },
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs
   }
 });
